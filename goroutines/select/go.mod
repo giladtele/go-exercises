ab1024f5b4bb20bc0afe8dec5a2c4f7dbae1dee1
giladtele <giladt@telemessage.com>
2024-03-17 17:19:17 +0200

    creating goroutines select program

---
+++ goroutines/select/go.mod
@@ -0,0 +1,3 @@
+module github.cogithub.com/giladtele/goroutines-select
+
+go 1.22.0
---
+++ goroutines/select/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func sendNums(numsChan chan int, wg *sync.WaitGroup) {
+	defer wg.Done()          // `defer` makes sure `wg.Done()` will only execute when the function nears its return phase
+	for i := 0; i < 4; i++ { // Loop 4 times to send 4 numbers
+		numsChan <- rand.Intn(100)
+	}
+}
+
+func sendStr(strChan chan string, wg *sync.WaitGroup) {
+	defer wg.Done() // `defer` makes sure `wg.Done()` will only execute when the function nears its return phase
+	strChan <- "hello"
+}
+
+func selectChans(numsChan chan int, strChan chan string) {
+	for {
+		select {
+		case num, ok := <-numsChan:
+			if !ok {
+				numsChan = nil
+			} else {
+				fmt.Println(num)
+			}
+		case str, ok := <-strChan:
+			if !ok {
+				strChan = nil
+			} else {
+				fmt.Println(str)
+			}
+		}
+		if numsChan == nil && strChan == nil {
+			break
+		}
+	}
+}
+
+func main() {
+	startTime := time.Now()
+
+	rand.Seed(time.Now().UnixNano()) // Ensure different random numbers each run
+	var wgNums, wgStr sync.WaitGroup
+	numsChan := make(chan int)
+	strChan := make(chan string)
+
+	go selectChans(numsChan, strChan)
+
+	for i := 0; i < 5; i++ { // Start 5 goroutines
+		wgNums.Add(1)
+		go sendNums(numsChan, &wgNums)
+	}
+
+	wgStr.Add(1)
+	go sendStr(strChan, &wgStr)
+
+	wgNums.Wait()
+	close(numsChan)
+
+	endTime := time.Now()
+
+	executionTime := endTime.Sub(startTime)
+	fmt.Printf("Program executed in%v\n", executionTime)
+}
